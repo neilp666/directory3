5d361a54797ce76f95567e99bb67f41cb1ed6373
neil patel <neil@myindianweddingapp.com>
2015-09-08 12:03:58 +0100

    Added profile scaffold

---
+++ app/views/profiles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @profile, :id, :description, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907183613) do
+ActiveRecord::Schema.define(version: 20150908092309) do
+
+  create_table "profiles", force: true do |t|
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
