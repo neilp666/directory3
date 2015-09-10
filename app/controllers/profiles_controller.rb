9e9694b95c93a66a51517134a19e7fe4d1a783d1
neil patel <neil@myindianweddingapp.com>
2015-09-10 20:13:36 +0100

    updated controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,7 +15,7 @@ class ProfilesController < ApplicationController
   end
 
   def new
-    @profile = current_user.profile.build
+    @profile = current_user.profiles.build
     respond_with(@profile)
   end
 
